9677224614059c54d696f92b471c06c504f852a4
krsnapaudel <dilli@alpenlabs.io>
2025-02-27 17:44:03 +0545

    mocked deposits and withdrawals

--- backend/src/bridge.rs
+++ backend/src/bridge.rs
@@ -60,6 +60,39 @@ pub struct OperatorStatus {
 #[derive(Debug, Deserialize)]
 struct OperatorPublicKeys(BTreeMap<u32, String>); // OperatorIdx -> PublicKey
 
+/// Enum to handle deposit and withdrawal operations without relying on a "type" field
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(untagged)]  // This allows Serde to determine the type based on the structure
+pub enum BridgeDuty {
+    Deposit(RpcDepositInfo),
+    Withdrawal(RpcWithdrawalInfo),
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct RpcDepositInfo {
+    deposit_request_outpoint: String,  // Convert OutPoint to String (txid:vout)
+    el_address: String,  // Convert Vec<u8> (EVM Address) to Hex String
+    total_amount: u64,   // Amount in satoshis
+    take_back_leaf_hash: String,  // Convert TapNodeHash to String
+    original_taproot_addr: String,  // Convert BitcoinAddress to String
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct RpcWithdrawalInfo {
+    deposit_outpoint: String,  // Convert OutPoint to String (txid:vout)
+    user_destination: String,  // Convert Descriptor to String
+    assigned_operator_idx: u32,  // Operator index as is
+    exec_deadline: u32,  // Bitcoin block height as is
+}
+
+/// Struct for `getBridgeDuties` Response
+#[derive(Debug, Serialize, Deserialize)]
+struct RpcBridgeDuties {
+    duties: Vec<BridgeDuty>,  // Mixed deposit & withdrawal duties
+    start_index: u64,
+    stop_index: u64,
+}
+
 #[derive(Debug, Clone, Deserialize, Serialize)]
 pub struct DepositInfo {
     pub deposit_request_txid: String,
@@ -115,13 +148,64 @@ pub async fn bridge_monitoring_task(state: BridgeState, config: &BridgeMonitorin
         interval.tick().await;
         let mut locked_state = state.lock().await;
 
-        let operators = get_bridge_operators(&config, &strata_rpc_client, &bridge_rpc_client).await.unwrap();
-        info!("operator status {}", operators.len());
-        locked_state.operators = operators;
+        // Bridge operator status
+        let operators = get_bridge_operators(&strata_rpc_client).await.unwrap();
+        let mut operator_statuses = Vec::new();
+        for (index, public_key) in operators.0.iter() {
+            let operator_id = format!("Alpen Labs #{}", index);
+            info!("operator {}", operator_id);
+            let status = get_operator_status(&config, &strata_rpc_client, public_key).await.unwrap();
+
+            operator_statuses.push(OperatorStatus {
+                operator_id,
+                operator_address: public_key.clone(),
+                status,
+            });
+        }
+
+        locked_state.operators = operator_statuses;
+
+        // Current deposits
+        let current_deposits = get_current_deposits(&bridge_rpc_client).await.unwrap();
+        info!("current deposits {}", current_deposits.len());
+
+        for deposit_id in current_deposits {
+            let deposit_info = get_deposit_info(&bridge_rpc_client, deposit_id).await.unwrap();
+            locked_state.deposits.push(deposit_info);
+        }
+
+        // Withdrawals
+        for (index, _) in operators.0.iter() {
+            let operator_id = format!("Alpen Labs #{}", index);
+            info!("operator {}", operator_id);
+            let mut withdrawal_infos: Vec<WithdrawalInfo> = match get_withdrawal_info(&strata_rpc_client, *index).await {
+                Ok(data) => data,
+                Err(e) => {
+                    error!("Bridge get withdrawal failed with {}", e);
+                    Vec::new()
+                }
+            };
+            locked_state.withdrawals.append(& mut withdrawal_infos);
+        }
     }
 }
 
-pub async fn get_bridge_operators(config: &BridgeMonitoringConfig, strata_rpc: &HttpClient, bridge_rpc: &HttpClient) -> Result<Vec<OperatorStatus>, ClientError> {
+// Mock operator data for testing
+fn mock_operator_table() -> OperatorPublicKeys {
+    let mut operator_table = OperatorPublicKeys {
+        0: BTreeMap::new(),
+    };
+
+    for i in 1..=3 {
+        operator_table.0.insert(i, format!("0xdeadbeef{}", i).to_string());
+    }
+
+    operator_table
+}
+
+pub async fn get_bridge_operators(rpc_client: &HttpClient) -> Result<OperatorPublicKeys, ClientError> {
+    let operator_table = mock_operator_table();
+
     // Fetch active operator public keys
     // let operator_table: OperatorPublicKeys = match strata_rpc.request("getActiveOperatorChainPubkeySet", ((),)).await {
     //     Ok(data) => data,
@@ -131,68 +215,120 @@ pub async fn get_bridge_operators(config: &BridgeMonitoringConfig, strata_rpc: &
     //     }
     // };
 
-    let mut statuses = Vec::new();
-
-    // for (index, public_key) in operator_table.0.iter() {
-    //     let operator_id = format!("Alpen Labs #{}", index);
-    //     info!("operator {}", operator_id);
-
-        // Check if operator responds to an RPC request
-        // Explicitly define return type as `bool`
-        // let ping_result: Result<bool, ClientError> =
-        //     timeout(Duration::from_secs(config.bridge_operator_ping_timeout_s), bridge_rpc.request("pingOperator", (public_key.clone(),)))
-        //         .await
-        //         .map_err(|_| ClientError::Custom("Timeout".into()))?;
-
-        // let status = if ping_result.is_ok() && ping_result.unwrap() {
-        //     "Online".to_string()
-        // } else {
-        //     "Offline".to_string()
-        // };
-
-    //     statuses.push(OperatorStatus {
-    //         operator_id,
-    //         operator_address: public_key.clone(),
-    //         status: "Online".to_string(),
-    //     });
-    // }
-
-    // Mock status for now
-    for i in 1..=3 {
-        statuses.push(OperatorStatus {
-            operator_id: format!("Alpen Labs #{}", i),
-            operator_address: format!("0xdeadbeef{}", i),
-            status: "Online".to_string(),
-        })
-    }
-
-    Ok(statuses)
+    Ok(operator_table)
 }
 
+pub async fn get_operator_status(config: &BridgeMonitoringConfig, rpc_client: &HttpClient, operator_pk: &String) -> Result<String, ClientError> {
+    // Check if operator responds to an RPC request
+    // Explicitly define return type as `bool`
+    // let ping_result: Result<bool, ClientError> =
+    //     timeout(Duration::from_secs(config.bridge_operator_ping_timeout_s), rpc_client.request("pingOperator", (operator_pk.clone(),)))
+    //         .await
+    //         .map_err(|_| ClientError::Custom("Timeout".into()))?;
 
-// pub async fn get_current_deposits() -> Result<Vec<String>, reqwest::Error> {
-//     let client = Client::new();
-//     let rpc_url = env::var("STRATA_RPC_URL").unwrap();
-//     let response = client.get(format!("{}/getCurrentDeposits", rpc_url)).send().await?;
-//     let deposit_indexes: Vec<String> = response.json().await?;
-//     Ok(deposit_indexes)
-// }
+    // let status = if ping_result.is_ok() && ping_result.unwrap() {
+    //     "Online".to_string()
+    // } else {
+    //     "Offline".to_string()
+    // };
 
-// pub async fn get_deposit_info(deposit_txid: &str) -> Result<DepositInfo, reqwest::Error> {
-//     let client = Client::new();
-//     let rpc_url = env::var("BRIDGE_ORCHESTRATOR_URL").unwrap();
-//     let response = client.get(format!("{}/getDepositInfo/{}", rpc_url, deposit_txid)).send().await?;
-//     let deposit_info: DepositInfo = response.json().await?;
-//     Ok(deposit_info)
-// }
+    // Ok(status)
 
-// pub async fn get_withdrawal_info(outpoint: &str) -> Result<Option<WithdrawalInfo>, reqwest::Error> {
-//     let client = Client::new();
-//     let rpc_url = env::var("BRIDGE_ORCHESTRATOR_URL").unwrap();
-//     let response = client.get(format!("{}/getWithdrawalInfo/{}", rpc_url, outpoint)).send().await?;
-//     let withdrawal_info: Option<WithdrawalInfo> = response.json().await?;
-//     Ok(withdrawal_info)
-// }
+    Ok("Online".to_string())
+}
+
+pub async fn get_current_deposits(rpc_client: &HttpClient) -> Result<Vec<u32>, ClientError> {
+    let deposit_ids = vec![1, 2, 3];
+    // let deposit_ids: Vec<u32> = match rpc_client.request("getCurrentDeposits", ((),)).await {
+    //     Ok(data) => data,
+    //     Err(e) => {
+    //         error!("Current deposits query failed with {}", e);
+    //         return Err(e);
+    //     }
+    // };
+
+    Ok(deposit_ids)
+}
+
+fn mock_deposit_info(deposit_id: u32) -> DepositInfo {
+    let deposit_txid = format!("0xaabbccddee{}", deposit_id);
+
+    let deposit_info = DepositInfo {
+        deposit_request_txid: format!("0xabcdefgh{}", deposit_id).into(),
+        deposit_txid: Some(deposit_txid.into()),
+        mint_txid: Some(format!("0xqrstuvwx{}", deposit_id).into()),
+        status: "Accepted".to_string(),
+    };
+
+    deposit_info
+}
+
+pub async fn get_deposit_info(rpc_client: &HttpClient, deposit_id: u32) -> Result<DepositInfo, ClientError> {
+
+    let deposit_info = mock_deposit_info(deposit_id);
+
+    // let deposit_txid: String = match rpc_client.request("getCurrentDepositById", (deposit_id,)).await {
+    //     Ok(data) => data,
+    //     Err(e) => {
+    //         error!("Get deposit by id failed with {}", e);
+    //         return Err(e);
+    //     }
+    // };
+;
+    // let deposit_info: DepositInfo = match rpc_client.request("getDepositInfo", (deposit_txid,)).await {
+    //     Ok(data) => data,
+    //     Err(e) => {
+    //         error!("Get deposit by id failed with {}", e);
+    //         return Err(e);
+    //     }
+    // };
+
+    Ok(deposit_info)
+}
+
+fn mock_withdrawal_info(operator_idx: u32) -> Vec<WithdrawalInfo> {
+    let mut withdrawals = Vec::new();
+    withdrawals.push(WithdrawalInfo {
+        withdrawal_request_txid: format!("0xaabbccddee{}", operator_idx).to_string(),
+        fulfillment_txid: Some(format!("0xffcdbade{}", operator_idx).to_string()),
+    status: "Accepted".to_string(),
+    });
+
+    withdrawals
+}
+
+pub async fn get_withdrawal_info(rpc_client: &HttpClient, operator_idx: u32) -> Result<Vec<WithdrawalInfo>, ClientError> {
+    let withdrawal_infos = mock_withdrawal_info(operator_idx);
+
+    // let bridge_duties: RpcBridgeDuties = match rpc_client.request("getBridgeDuties", (operator_idx, 0)).await {
+    //     Ok(data) => data,
+    //     Err(e) => {
+    //         error!("Get bridge duties failed with {}", e);
+    //         return Err(e);
+    //     }
+    // };
+
+    // let mut withdrawal_infos = Vec::new();
+    // for duty in bridge_duties.duties.iter() {
+    //     if let BridgeDuty::Withdrawal(withdrawal_info) = duty {
+    //         let deposit_outpoint = withdrawal_info.deposit_outpoint.clone();
+    //         println!("Calling getWithdrawalInfo for Outpoint: {}", deposit_outpoint);
+
+    //         // Call `getWithdrawalInfo(deposit_outpoint)` here
+    //         let wd_info: WithdrawalInfo = match rpc_client.request("getWithdrawalInfo", (deposit_outpoint.clone(), )).await {
+    //             Ok(data) => data,
+    //             Err(e) => {
+    //                 error!("Get withdrawal info failed with {}", e);
+    //                 return Err(e);
+    //             }
+    //         };
+
+    //         withdrawal_infos.push(wd_info);
+    //     }
+    // }
+
+    Ok(withdrawal_infos)
+}
 
 // pub async fn get_claim_info(claim_txid: &str) -> Result<ReimbursementInfo, reqwest::Error> {
 //     let client = Client::new();
--- frontend/src/pages/Bridge.tsx
+++ frontend/src/pages/Bridge.tsx
@@ -1,6 +1,12 @@
 import { Suspense } from "react";
 import { useLocation } from "react-router-dom";
-import { useBridgeStatus, OperatorStatus } from "../hooks/useBridgeStatus";
+import { useBridgeStatus, OperatorStatus, DepositInfo, WithdrawalInfo } from "../hooks/useBridgeStatus";
+
+const truncateTxid = (txid: string, startLength = 6, endLength = 6) => {
+    if (!txid) return "-"; // If no TXID, show "-"
+    if (txid.length <= startLength + endLength) return txid; // If short, return as is
+    return `${txid.slice(0, startLength)}...${txid.slice(-endLength)}`;
+};
 
 export default function Bridge() {
     const { pathname } = useLocation(); // Get current URL path
@@ -11,33 +17,89 @@ export default function Bridge() {
         <div className="bridge-content">
             {/* Bridge Status Page */}
             {pathname === "/bridge" && (
-                <div className="bridge-container">
+                <div>
                     {!data || error ? <p className="error-text">Error loading data</p> : null}
                     <Suspense fallback={<p className="loading-text">Loading...</p>}>
                         {isLoading ? (
                             <p className="loading-text">Loading...</p>
                         ) : (
-                            <div className="operators-section">
-                                <span className="operators-title">Bridge operator status</span>
-                                { data && data.operators ? (
-                                    <div className="table-wrapper">
-                                        <table className="operators-table">
-                                            <tbody>
-                                                {data.operators.map((operator: OperatorStatus, index: number) => (
-                                                    <tr key={index} className="operators-row">
-                                                        <td className="operator-status">{operator.operator_id}</td>
-                                                        <td className="operator-status">{operator.operator_address}</td>
-                                                        <td className={`operator-status ${operator.status.toLowerCase()}`}>
-                                                            {operator.status}
-                                                        </td>
+                            <div className="bridge-container">
+                                <div className="bridge-section">
+                                    <span className="bridge-title">Bridge operator status</span>
+                                    { data && data.operators ? (
+                                        <div className="table-wrapper">
+                                            <table className="operators-table">
+                                                <tbody>
+                                                    {data.operators.map((operator: OperatorStatus, index: number) => (
+                                                        <tr key={index} className="operators-row">
+                                                            <td className="table-cell">{operator.operator_id}</td>
+                                                            <td className="table-cell">{operator.operator_address}</td>
+                                                            <td className="table-cell">{operator.status}</td>
+                                                        </tr>
+                                                    ))}
+                                                </tbody>
+                                            </table>
+                                        </div>
+                                    ) : (
+                                        <p className="no-items">No bridge operators found.</p>
+                                    )}
+                                </div>
+                                <div className="bridge-section">
+                                    <span className="bridge-title">Bridge deposit status</span>
+                                    { data && data.deposits ? (
+                                        <div className="table-wrapper">
+                                            <table className="transactions-table">
+                                                <thead>
+                                                    <tr className="transactions-header">
+                                                        <th>Deposit Request TXID</th>
+                                                        <th>Deposit TXID</th>
+                                                        <th>Mint TXID</th>
+                                                        <th>Status</th>
                                                     </tr>
-                                                ))}
-                                            </tbody>
-                                        </table>
-                                    </div>
-                                ) : (
-                                    <p className="no-items">No bridge operators found.</p>
-                                )}
+                                                </thead>
+                                                <tbody>
+                                                    {data.deposits.map((deposit_info: DepositInfo, index: number) => (
+                                                        <tr key={index} className="transactions-row">
+                                                            <td className="table-cell">{truncateTxid(deposit_info.deposit_request_txid)}</td>
+                                                            <td className="table-cell">{truncateTxid(deposit_info.deposit_txid)}</td>
+                                                            <td className="table-cell">{truncateTxid(deposit_info.mint_txid)}</td>
+                                                            <td className="table-cell">{deposit_info.status}</td>
+                                                        </tr>
+                                                    ))}
+                                                </tbody>
+                                            </table>
+                                        </div>
+                                    ) : (
+                                        <p className="no-items">No bridge deposits found.</p>
+                                    )}
+                                </div>
+                                <div className="bridge-section">
+                                    <span className="bridge-title">Bridge withdrawal status</span>
+                                    { data && data.withdrawals ? (
+                                        <div className="table-wrapper">
+                                            <table className="transactions-table">
+                                                <thead>
+                                                    <tr className="transactions-header">
+                                                        <th>Withdrawal Request TXID</th>
+                                                        <th>Fulfillment TXID</th>
+                                                        <th>Status</th>
+                                                    </tr>
+                                                </thead>
+                                                <tbody>
+                                                    {data.withdrawals.map((withdrawal_info: WithdrawalInfo, index: number) => (
+                                                        <tr key={index} className="transactions-row">
+                                                            <td className="table-cell">{truncateTxid(withdrawal_info.withdrawal_request_txid)}</td>
+                                                            <td className="table-cell">{truncateTxid(withdrawal_info.fulfillment_txid)}</td>
+                                                            <td className="table-cell">{withdrawal_info.status}</td>
+                                                        </tr>
+                                                    ))}
+                                                </tbody>
+                                            </table>
+                                        </div>
+                                    ) : (
+                                        <p className="no-items">No bridge withdrawals found.</p>
+                                    )}
+                                </div>
                             </div>
                         )}
                     </Suspense>
