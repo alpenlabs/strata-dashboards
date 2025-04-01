f92bdec028063899384b37ae637cccea49fd9445
krsnapaudel <dilli@alpenlabs.io>
2025-04-01 12:23:58 +0800

    add staging hostname

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,8 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   server: {
-    allowedHosts: ["dashboard.testnet.alpenlabs.io"]
+    allowedHosts: ["dashboard.testnet.alpenlabs.io"],
+    allowedHosts: ["dashboard.testnet-staging.stratabtc.org"],
   },
 });
 
